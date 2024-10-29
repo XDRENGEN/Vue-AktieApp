eb79cfdf736656823fa51019ba5a424a8da7f019
XDRENGEN <149380097+XDRENGEN@users.noreply.github.com>
2024-10-29 13:26:39 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            intro: 'Welcome to my vue template',
+        }
+    },
+
+
+    computed: {
+        myComputed(){
+            return''
+        }
+    },
+
+
+    methods: {
+        myMethod(){
+        
+        }
+    },
+
+
+}).mount('#app');
